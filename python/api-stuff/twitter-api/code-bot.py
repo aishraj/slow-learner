5432c3ab7ecc0220a6c33219c78706b9ce003dae
AR Dahal <dahalaishraj@gmail.com>
2013-02-04 18:45:42 +0530

    Added DSIC-Code bot

---
+++ python/api-stuff/twitter-api/code-bot.py
@@ -0,0 +1,34 @@
+"""
+
+This is the source code of a bare bone Twitter Bot that uses the Twitter Python Library.
+It finds users who tweeted about "Dell Social Innovation Challenge", favourites their tweets,
+follows them and then sends them a tweet with a link to Project Code - an initiative to teach kids
+programming at an early age.
+
+"""
+
+import urllib
+import simplejson
+import twitter
+
+consumer_key = 'YOUR_KEY_HERE'
+consumer_secret = 'YOUR SECRET HERE'
+access_token_key = 'YOUR TOKEN HERE'
+access_token_secret = 'YOUR TOKEN SECRET HERE'
+
+def searchTweets(query):
+    search = urllib.urlopen('http://search.twitter.com/search.json?q='+query)
+    dict = simplejson.loads(search.read())
+    return dict
+
+api = twitter.Api(consumer_key = consumer_key, consumer_secret = consumer_secret, access_token_key = access_token_key, access_token_secret = access_token_secret)
+tweets = searchTweets("dell+social+innovation+challenge&rpp=100&result_type=recent")
+msg = 'Hello there! Saw your tweet about DSIC. Will be thankful if you could vote for Code at http://goo.gl/MhMmJ'
+
+for i in range(len(tweets["results"])):
+    tweeter = tweets["results"][i]["from_user"]
+    status = twitter.Api.GetStatus(api, tweets["results"][i]["id"])
+    api.CreateFavorite(status)
+    api.CreateFriendship(tweeter)
+    api.PostUpdate('@' + tweeter + ' ' + msg)
+
