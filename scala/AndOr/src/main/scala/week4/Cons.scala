8b1d14aba014cfc85fe3612b1d495639f0d608b5
ge3k <ge3k@users.noreply.github.com>
2014-05-24 10:11:16 +0530

    apply method - functions as objects

--- scala/AndOr/src/main/scala/week4/Cons.scala
+++ scala/AndOr/src/main/scala/week4/Cons.scala
@@ -6,7 +6,7 @@ import java.util.NoSuchElementException
  * Created by ge3k on 17/5/14.
  */
 
-class Cons[T] (val head: T, val tail : List[T]) extends List{
+class Cons[T] (val head: T, val tail : List[T]) extends List[T]{
   override def isEmpty: Boolean = false
 }
 
@@ -16,4 +16,9 @@ class Nil[T] extends List[T] {
   override def tail: Nothing = throw new NoSuchElementException("Nil head")
 
   override def head: Nothing = throw new NoSuchElementException("Nil head")
+}
+
+object List {
+  def apply[T](x1: T, x2: T) = List[T] = new Cons(x1,new Cons(x2, new Nil))
+  def apply[T]() = new Nil
 }
